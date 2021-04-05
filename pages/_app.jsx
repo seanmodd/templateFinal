6f54f1e4d3477a7a137b35747dc263b481205f43
Sean <sean@vestaridefair.com>
2021-04-05 13:26:28 -0700

    now TEMPLATE

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeProvider, Heading } from '@chakra-ui/react';
 import '../styles/globals.css';
 import { GoogleFonts } from 'next-google-fonts';
 import theme from '../styles/theme';
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         }}
       >
         <Component {...pageProps} />
+        <Heading>Here</Heading>
       </ColorModeProvider>
       <GoogleFonts href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
     </ChakraProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,18 @@
-import { Button } from '@chakra-ui/button';
-import { Heading, Text } from '@chakra-ui/layout';
-import Head from 'next/head';
-import styled from 'styled-components';
-import { useColorModeValue as mode } from '@chakra-ui/color-mode';
+import { Heading } from '@chakra-ui/layout'
+import type { TypeA } from '../types'
+import { GoogleFonts } from 'next-google-fonts';
+const Example: TypeA = {
+  name: 'next',
+}
 
-const MyHeading = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  font-family: Poppins;
-  color: palevioletred;
-`;
-const hoverStyle = {
-  boxShadow: '7px 7px 7px 7px  rgba(223, 3, 172, 0.2)',
-  textShadow: '1px 1px #ff0000',
-  transition: 'all .2s ease-out',
-  background: '#62ff00',
-  color: 'black',
-  fontWeight: '400',
-};
-const pressedStyle = {
-  background: '#0d00ff',
-  color: '#ffff00',
-  transition: 'all 0.6s ease-out',
-  textShadow: '6px 6px #ff00ae',
-  fontWeight: '400',
-};
-const MyButton = ({ children }) => (
-  <div>
-    <Button
-      transition="0.5s"
-      boxShadow="3px 3px 3px 3px rgba(0, 0, 255, 0.2)"
-      bg={mode('gray.700', 'gray.100')}
-      color={mode('gray.100', 'gray.700')}
-      p="10px"
-      m="10px"
-      _hover={hoverStyle}
-      _active={pressedStyle}
-    >
-      {children}
-    </Button>
-  </div>
-);
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-      <main className="main">
-        <Heading>Here</Heading>
-
-        <MyHeading>MyHeading</MyHeading>
-        <h1 className="title">
-          <MyButton>Sample Button</MyButton>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <Text fontSize="40px">Sample Text from Chakra UI</Text>
-        <Text>Here</Text>
-      </main>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <h1>
+        <p>{Example.name}</p>
+        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
+        <Heading fontFamily="Poppins">Here</Heading>
+      </h1>
     </div>
-  );
+  )
 }
